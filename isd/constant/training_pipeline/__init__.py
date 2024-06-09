63bd5f599d64933db4b81668588bb98c12374be7
Sourangshu Pal <paulbindass@gmail.com>
2024-06-09 10:03:14 +0530

    bucket updated

--- isd/constant/training_pipeline/__init__.py
+++ isd/constant/training_pipeline/__init__.py
@@ -10,7 +10,7 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_INGESTION_S3_DATA_NAME: str = "isd_data_mini.zip"
 
-DATA_BUCKET_NAME = "isd-data-3025"
+DATA_BUCKET_NAME = "isd-dataset"
 
 
 
@@ -43,6 +43,6 @@ MODEL_TRAINER_BATCH_SIZE: int = 8
 """
 MODEL PUSHER related constant start with MODEL_PUSHER var name
 """
-MODEL_BUCKET_NAME = "isd-data-24"
+MODEL_BUCKET_NAME = "isd-dataset"
 S3_MODEL_NAME = "best.pt"
 
